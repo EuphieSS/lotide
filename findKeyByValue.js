128fbee6c79e14080759495f5fb3a50cf7462706
EuphieSS <samma118@hotmail.com>
2023-01-12 14:46:11 +0000

    Initial commitment

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+//FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//FIND_KEY_BY_VALUE FUNCTION
+const findKeyByValue = function(object, value) {
+  for (let property in object) {
+    if (value === object[property]) {
+      return property;
+    }
+  }
+};
+
+//TEST CODE
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
