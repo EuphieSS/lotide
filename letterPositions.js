4bbfc482553bb83413842cbe66eb69445a34c057
EuphieSS <samma118@hotmail.com>
2023-01-12 14:59:56 +0000

    Initial commitment

---
+++ letterPositions.js
@@ -0,0 +1,47 @@
+//ARRAY COMPARISON FUNCTION
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === 0 && arr2.length === 0) {
+    return true;
+  }
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    let finalOutput = false;
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      } else {
+        finalOutput = true;
+      }
+    }
+    return finalOutput;
+  }
+}
+
+//ASSERTION FUNCTION FOR ARRAY COMPARISON
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+//LETTERPOSITIONS FUNCTION
+const letterPositions = function(sentence) {
+  const results = {};
+  sentence = sentence.toLowerCase();
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (!results[sentence[i]]) {
+        results[sentence[i]] = [];
+      }
+      results[sentence[i]].push(i);
+    }
+  }
+  return results;
+}
+
+//TEST CASE
+assertArraysEqual(letterPositions("helLo").l, [2, 3]);
